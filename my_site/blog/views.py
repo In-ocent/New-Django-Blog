9fe5427d30f7ce7c2e4f88c98ec465e3f5148731
ino-cent <paulinnocentojochenemi@gmail.com>
2026-02-05 16:59:19 +0100

    update

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,12 +1,6 @@
-from django.shortcuts import render
-from datetime import date
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from . models import Post
 # Create your views here.
-
-all_posts = [
-
-]
 
 
 def starting_page(request):
@@ -15,17 +9,12 @@ def starting_page(request):
 
 
 def posts(request):
-    all_posts = Post.objects.all()
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
 
 
 def post_detail(request, slug):
-    identiend_post = next(
-        (post for post in all_posts if post['slug'] == slug), None)
-
-    if identiend_post is None:
-        return render(request, "404.html", status=404)
-
+    identiend_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog\post-detali.html", {"post": identiend_post})
