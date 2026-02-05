f454edb890c468501a15ffbcaefa0c73127b8e6f
ino-cent <paulinnocentojochenemi@gmail.com>
2026-02-05 15:39:04 +0100

    update

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name} {self.email_address}"
+
+    def __str__(self):
+        return self.full_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, db_index=True)
+    image_name = models.CharField(max_length=50)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, related_name='posts', null=True)
+    tags = models.ManyToManyField(Tag)
+    date = models.DateField(auto_now=True)
+
+    def full_name(self):
+        return f"{self.title} {self.excerpt} {self.author}"
+
+    def __str__(self):
+        return self.full_name()
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,54 +1,21 @@
 from django.shortcuts import render
 from datetime import date
 from django.http import Http404
-
+from . models import Post
 # Create your views here.
 
 all_posts = [
-    {
-        "slug": "hike-the-mountains",
-        "image": "forest.png",
-        "author": "InnoCoda",
-        "date": date(2026, 1, 19),
-        "title": "Dasert",
-        "excerpt": "There's nothing like the view, you get when you getto the top of hiking moutain",
-        "content": """My goal is to keep on growing as a developer - and if i could 
-                                help you do the same, I'd be very happy!""",
-    },
-    {
-        "slug": "programing",
-        "image": "innocoda.png",
-        "author": "InnoCoda",
-        "date": date(2024, 1, 9),
-        "title": "God's Wills",
-        "excerpt": "They is all ways God by my side all time ",
-        "content": """ My goal is to keep on growing as a developer - and if i could 
-                                help you do the same, I'd be very happy!""",
-    },
-    {
-        "slug": "Coding-is-life",
-        "image": "coding.png",
-        "author": "InnoCoda",
-        "date": date(2025, 1, 20),
-        "title": "Coding is life",
-        "excerpt": "There's nothing like seeing your code making you money ",
-        "content": """ My goal is to keep on growing as a developer - and if i could 
-                                help you do the same, I'd be very happy!""",
-    }
+
 ]
 
 
-def get_date(post):
-    return post['date']
-
-
 def starting_page(request):
-    sorted_post = sorted(all_posts, key=get_date)
-    latest_posts = sorted_post[-3:]
+    latest_posts = Post.objects.all().order_by('date')[:3]
     return render(request, "blog/index.html", {"posts": latest_posts})
 
 
 def posts(request):
+    all_posts = Post.objects.all()
     return render(request, "blog/all-posts.html", {
         "all_posts": all_posts
     })
